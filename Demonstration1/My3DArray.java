326415f968239483145645afd48592ce5ac3bf12
suhelkhanca <suhel.hata@gmail.com>
2024-01-21 18:41:34 +0530

    #D array implementation

---
+++ Demonstration1/My3DArray.java
@@ -0,0 +1,24 @@
+package Demonstration1;
+
+public class My3DArray {
+    public static void main(String[] args) {
+        int[][][] my3D = new int[3][4][5];
+        // Initialization
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 5; k++) {
+                    my3D[i][j][k] = i*j*k;
+                }
+            }
+        }
+        // Traversing 
+        for (int i = 0; i < my3D.length; i++) {
+            for (int j = 0; j < my3D[i].length; j++) {
+                for (int k = 0; k < my3D[j].length; k++) {
+                    System.out.println(my3D[i][j][k]);
+                }
+            }
+        }
+
+    }
+}

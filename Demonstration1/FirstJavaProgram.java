ee05986462354c72f13e8d7390f1279d1cdcd56c
suhelkhanca <suhel.hata@gmail.com>
2024-01-21 18:14:10 +0530

    First Java Program

---
+++ Demonstration1/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+package Demonstration1;
+class FirstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("Congratulation !, Your first JAVA program runs successfully.");
+    }
+}

a41b106e86436893657e6e48ac7de3b527754d6b
suhelkhanca <suhel.hata@gmail.com>
2024-01-27 06:36:31 +0530

    Hello world applet

--- Demonstration2/HelloWorldApplet.java
+++ Demonstration2/HelloWorldApplet.java
@@ -1,7 +1,7 @@
 package Demonstration2;
 import java.applet.Applet;
 import java.awt.Graphics;
-public class HelloWorldApplet extends Applet {
+public class HelloWorldApplet extends Applet { // Basic applet program
     public void paint(Graphics g){
         g.drawString("Hello World", 150, 150);
     }

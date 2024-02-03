df448376e9dcca14f5ba61684e95e047927081f3
suhelkhanca <suhel.hata@gmail.com>
2024-02-03 20:05:27 +0530

    CLI input

--- Practice2DArrays/CommandLineInp.java
+++ Practice2DArrays/CommandLineInp.java
@@ -1,7 +1,6 @@
-package Practice2Darrays;
 public class CommandLineInp{ // taking input from command line
     public static void main(String[] x){
-        System.out.println("Hello ! ");
+        System.out.print("Hello ! ");
         System.out.println(x[0]);
     }
 }

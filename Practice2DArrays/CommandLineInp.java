ab4d7c4bb6b73e4d300f6f01253857425c5e07ab
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-02-03 14:20:41 +0000

    taking input from command line

---
+++ Practice2DArrays/CommandLineInp.java
@@ -0,0 +1,7 @@
+package Practice2Darrays;
+public class CommandLineInp{ // taking input from command line
+    public static void main(String[] x){
+        System.out.println("Hello ! ");
+        System.out.println(x[0]);
+    }
+}

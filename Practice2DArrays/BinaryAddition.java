36b78e3a51b7e25a75091b911e080f590e696599
suhelkhanca <suhel.hata@gmail.com>
2024-02-09 14:39:19 +0530

    Binary Addition of two numbers

---
+++ Practice2DArrays/BinaryAddition.java
@@ -0,0 +1,9 @@
+package Practice2DArrays;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        int a = 0b10111;
+        int b = 0b0111;
+        System.out.println("Binary Addition of 'a' and 'b' is: " + Integer.toBinaryString((a+b)));
+    }
+}

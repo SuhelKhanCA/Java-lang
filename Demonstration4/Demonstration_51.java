354f1962163f1cc4bdfe02e332bcfd8dabf95d99
suhelkhanca <suhel.hata@gmail.com>
2024-02-09 15:13:00 +0530

    printing 10 odd numbers using (2n -1)

---
+++ Demonstration4/Demonstration_51.java
@@ -0,0 +1,12 @@
+package Demonstration4;
+
+public class Demonstration_51 { // printing 10 odd numbers using (2n -1)
+    public static void main(String[] args) {
+        int count = 1;
+        System.out.println("---Printing first 10 odd numbers---");
+        while (count < 11) {
+            System.out.print(" " + (2*count - 1));
+            count++;
+        }
+    }
+}

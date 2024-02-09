d1f1734fd17c2423e752a0fa4d1707336dcf5c4e
suhelkhanca <suhel.hata@gmail.com>
2024-02-09 15:20:22 +0530

    a simple for loop

---
+++ Demonstration4/Demonstration_53.java
@@ -0,0 +1,9 @@
+package Demonstration4;
+
+public class Demonstration_53 { // a simple for loop
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Count is: " + i);
+        }
+    }
+}

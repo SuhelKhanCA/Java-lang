3e3378b55b6b18c8b7dc3841446a242ee4edcabf
suhelkhanca <suhel.hata@gmail.com>
2024-02-09 14:53:16 +0530

    constructor reusing example

---
+++ Demonstration3/Demonstration_310.java
@@ -0,0 +1,11 @@
+package Demonstration3;
+
+public class Demonstration_310 {
+    public static void main(String[] args) {
+        Student s1 = new Student(111, "Ankit", "Java", 3.5F);
+        Student s2 = new Student(112, "Sumit", "Java");
+
+        s1.display();
+        s2.display();
+    }
+}

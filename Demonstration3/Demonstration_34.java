6d6d12dca73e712eea55255a9b6f296160956e21
suhelkhanca <suhel.hata@gmail.com>
2024-02-09 12:53:55 +0530

    a class with method

---
+++ Demonstration3/Demonstration_34.java
@@ -0,0 +1,17 @@
+package Demonstration3;
+class Point{
+    int x;
+    int y;
+}
+public class Demonstration_34 {
+    public static void main(String[] args) {
+        Point myPoint = new Point(); // Declaring an object of class Point
+        // Assign value to myPoint's instance variable
+        myPoint.x = 10;
+        myPoint.y = 20;
+
+        System.out.println(" x  = " + myPoint.x);
+        System.out.println(" y = " + myPoint.y);
+        
+    }
+}

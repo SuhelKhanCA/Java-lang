430c4f5881d0419cc7140d5db689968f32456091
suhelkhanca <suhel.hata@gmail.com>
2024-02-11 15:51:41 +0530

    previous program after correcting the error: scope rule

---
+++ Demonstration5/Demonstration_510.java
@@ -0,0 +1,11 @@
+package Demonstration5;
+
+public class Demonstration_510 { // previous program after correcting the error: scope rule
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i<4; i++){
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+}

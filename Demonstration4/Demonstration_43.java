dcbbbaaa2a0c7356b0f0374a0675c768cc49b232
suhelkhanca <suhel.hata@gmail.com>
2024-02-11 16:16:04 +0530

    System.exit(0); can be useful in larger programs where you might want to stop execution in places other than the end of the main method. For example, you might want to stop the program if a critical error occurs, or if a certain condition is met.

--- Demonstration4/Demonstration_43.java
+++ Demonstration4/Demonstration_43.java
@@ -5,6 +5,18 @@ public class Demonstration_43 { // CLI input in java
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i] + " ");
         }
-        System.exit(0);
+        System.exit(0); //  statement in Java is used to terminate the currently running Java Virtual Machine (JVM).
     }
 }
+/*
+ * public class Example {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No arguments provided!");
+            System.exit(1); // Exit with a status code of 1 to indicate an error
+        }
+        // Rest of the program...
+    }
+}
+
+ */

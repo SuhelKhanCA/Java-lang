73d348c1dc470a32a4a45fd12a29b63bec82935f
suhelkhanca <suhel.hata@gmail.com>
2024-02-11 16:02:29 +0530

    2nd program to show working of print() and println()

---
+++ Demonstration4/Demonstration_42.java
@@ -0,0 +1,12 @@
+package Demonstration4;
+
+public class Demonstration_42 { // 2nd program to show working of print() and println()
+    public static void main(String[] args) {
+        System.out.println("1. println");
+        System.out.println("2. println");
+
+        System.out.print("1. print");
+        System.out.print("\n");
+        System.out.print("2. print" + "3. print");
+    }
+}

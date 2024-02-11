46850b16ff462a9a9a2f39ca9941abe5a4fecca6
suhelkhanca <suhel.hata@gmail.com>
2024-02-11 15:49:15 +0530

    scope rule

---
+++ Demonstration5/Demonstration_59.java
@@ -0,0 +1,11 @@
+package Demonstration5;
+
+public class Demonstration_59 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+        }
+        // this line will produce error
+        // System.out.println(i);
+    }
+}

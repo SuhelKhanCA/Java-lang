b67eb3f9875ccff9271d1f0560ae8c3e926cdf74
suhelkhanca <suhel.hata@gmail.com>
2024-02-11 16:13:06 +0530

    CLI input in java

---
+++ Demonstration4/Demonstration_43.java
@@ -0,0 +1,10 @@
+package Demonstration4;
+
+public class Demonstration_43 { // CLI input in java
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i] + " ");
+        }
+        System.exit(0);
+    }
+}

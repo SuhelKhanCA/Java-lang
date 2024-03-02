efb2e86f5d0b4545e12c7d0322bdcc890e1c3e00
suhelkhanca <suhel.hata@gmail.com>
2024-03-02 11:51:35 +0530

    Error in the program

---
+++ Demonstration10/Error.java
@@ -0,0 +1,19 @@
+package Demonstration10;
+
+public class Error {
+    // Public static void main(String[] args){
+    //     System.out.println("Can you find errors in me...???");
+    // }
+}
+class AnotherError{
+    public void insert(){
+        System.out.println("To insert a text");
+    }
+    // abstract void delete(){ // should be defined in the abstract class only
+    //     System.out.println("TO delete a text");
+    // }
+}
+/*
+ * NOTE:
+ * A class file can be compiled succesfully, if it is syntactically correct, even there is no main class
+ */

424c4ca39902ad96fe1ec563e79657b5faafcda1
suhelkhanca <suhel.hata@gmail.com>
2024-03-02 13:57:22 +0530

    final class example

---
+++ Demonstration6b/Demonstration_611.java
@@ -0,0 +1,10 @@
+package Demonstration6b;
+final class Bike{}
+// class Honda1 extends Bike{ // Error : the type Honda1 cannot subclass the final class Bike
+
+// }
+public class Demonstration_611 {
+    public static void main(String[] args) {
+        
+    }
+}

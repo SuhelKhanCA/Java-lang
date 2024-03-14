dd36ec02332676051ca6af0a9b3bd6f5d602f9f2
suhelkhanca <suhel.hata@gmail.com>
2024-03-14 12:23:00 +0530

    simple example of package

---
+++ Demonstration7/pack1/A.java
@@ -0,0 +1,7 @@
+package Demonstration7.pack1;
+
+public class A {
+    void msg(){
+        System.out.println("Hi I'm in class A.");
+    }
+}
---
+++ Demonstration7/pack2/B.java
@@ -0,0 +1,8 @@
+package Demonstration7.pack2;
+
+public class B {
+    public static void main(String[] args) {
+        // A obj = new A(); // Compile time error
+        // obj.msg(); 
+    }
+}

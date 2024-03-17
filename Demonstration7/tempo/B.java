b77ac48b894719118fed24c47dad75e5443e69bc
suhelkhanca <suhel.hata@gmail.com>
2024-03-17 06:18:56 +0530

    creating a class in the tempo directory

---
+++ Demonstration7/tempo/B.java
@@ -0,0 +1,8 @@
+package Demonstration7.tempo;
+
+public class B {
+    public static void main(String[] args) {
+        A obj = new A(); // okay: accessible
+        obj.msg(); // it is also accesible
+    }
+}

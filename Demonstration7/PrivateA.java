d034cf8a2a51344decc3e4062f56e3ad5ff6ef30
suhelkhanca <suhel.hata@gmail.com>
2024-03-17 06:47:37 +0530

    example of private class access modifier

---
+++ Demonstration7/PrivateA.java
@@ -0,0 +1,18 @@
+package Demonstration7;
+
+/*private class PrivateAA { // illegal modifier for the class
+    int data = 40;
+    void msg(){
+        System.out.println("Class A : Hello Java!");
+    }
+}*/
+public class PrivateA{
+    public static void main(String[] args) {
+        // PrivateAA oobj = new PrivateAA(); // The constructor PrivateAA() is not visible
+        // System.out.println(oobj.data); // data is private
+        // oobj.msg(); // method is private 
+    }
+}
+/*
+ * When a class is 
+ */
---
+++ Demonstration7/PrivateAA.java
@@ -0,0 +1,8 @@
+package Demonstration7;
+
+// private class PrivateAA { // illegal modifier for the class
+//     int data = 40;
+//     void msg(){
+//         System.out.println("Class A : Hello Java!");
+//     }
+// }

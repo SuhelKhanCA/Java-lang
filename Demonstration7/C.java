4747376631d67d4a13204edfae97c6a37d9208cd
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-04-16 08:50:40 +0000

    making constructor private

---
+++ Demonstration7/C.java
@@ -0,0 +1,11 @@
+package Demonstration7;
+class C{
+    private C(){
+        // private constructor
+    }
+}
+class Simple{
+    public static void main(String[] args) {
+        // C obj  = new C(); //  private constructor cannt create an instance
+    }
+}
